d35021e2e736614d209750eee3457996f2d0beb2
Eshita Goel <112014636+eshita2210@users.noreply.github.com>
2024-07-17 19:31:47 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,16 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from "./ProjectCard";
-// import projImg1 from "../assets/img/project-img1.png";
-// import projImg2 from "../assets/img/project-img2.png";
-// import projImg3 from "../assets/img/project-img3.png";
+import React from 'react';
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCard';
 
-import notesImg from "../assets/img/notes.png";
-import TinDogImg from "../assets/img/TinDog.png";
-import DicEchallengeImg from "../assets/img/thisdice.png";
-import responsiveWebsiteImg from "../assets/img/responsiveWebsite.png";
-import socialImg from "../assets/img/social.png";
-import DrumkitImg from "../assets/img/drumkit 2.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
+import notesImg from '../assets/img/notes.png';
+import TinDogImg from '../assets/img/TinDog.png';
+import DicEchallengeImg from '../assets/img/thisdice.png';
+import shopperImg from '../assets/img/shopper.png';
+import weatherImg from '../assets/img/weather.png';
+
+import DrumkitImg from '../assets/img/drumkit 2.png';
+import colorSharp2 from '../assets/img/color-sharp2.png';
+
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -18,34 +18,44 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Social Media Web app",
-      description: "Design & Development",
-      imgUrl: socialImg,
+      title: 'Weather Predictor',
+      description: 'Design & Development',
+      imgUrl: weatherImg,
+      link: 'https://eshita2210.github.io/weather-app/'
     },
+
     {
-      title: "Notes Generator",
-      description: "Design & Development",
-      imgUrl: notesImg,
-    },
-    {
-      title: "TinDog",
-      description: "Design & Development",
-      imgUrl: TinDogImg,
-    },
-    {
-      title: "Dice challenge",
-      description: "Design & Development",
+      title: 'Dice challenge',
+      description: 'Design & Development',
       imgUrl: DicEchallengeImg,
+      link: 'https://eshita2210.github.io/dice-challenge/'
     },
     {
-      title: "responsive Website",
-      description: "Design & Development",
-      imgUrl: responsiveWebsiteImg,
+      title: 'Notes - Maker',
+      description: 'Design & Development',
+      imgUrl: notesImg,
+      link: 'https://eshita2210.github.io/notes-maker/'
     },
+
     {
-      title: "Drum kit",
-      description: "Design & Development",
+      title: 'SHOPPER-online shopping website',
+      description: 'Design & Development',
+      imgUrl: shopperImg,
+      link: 'https://eshita2210.github.io/shopper-website/'
+    },
+   
+   
+    {
+      title: 'Drum Kit',
+      description: 'Design & Development',
       imgUrl: DrumkitImg,
+      link: 'https://eshita2210.github.io/Drum-kit/'
+    },
+    {
+      title: 'TinDog',
+      description: 'Design & Development',
+      imgUrl: TinDogImg,
+      link: 'https://eshita2210.github.io/TinDog/'
     },
   ];
 
@@ -53,53 +63,50 @@ export const Projects = () => {
     <section className="project" id="projects">
       <Container>
         <Row>
-          <Col size={12}>
+          <Col xs={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Here are the various projects I have been developing using various tools and technologies.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Waiting for more projects!</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Always ready to collaborate.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div className={isVisible ? 'animate_animated animate_fadeIn' : ''}>
+                  <h2>Projects</h2>
+                  <p>Here are the various projects I have been developing using various tools and technologies.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? 'animate_animated animate_slideInUp' : ''}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          ))}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>Waiting for more projects!</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>Always ready to collaborate.</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="Background" />
     </section>
-  )
-}
+  );
+};
